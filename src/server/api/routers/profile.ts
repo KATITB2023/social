2df41b39f63e15184eea6cb95528bacc3c73f9c7
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-14 18:08:27 +0700

    downgrade prisma, update profile type

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,3 +1,15 @@
+import { type Profile } from "@prisma/client";
 import { createTRPCRouter } from "~/server/api/trpc";
+
+export type FRIENDSHIP_STATUS = "NOT_FRIEND" | "FRIEND" | "REQUESTED";
+
+export type SelfProfile = {
+  id: string;
+  nim: string;
+} & Omit<Profile, "updatedAt" | "userId">;
+
+export type UserProfile = SelfProfile & {
+  status: FRIENDSHIP_STATUS;
+};
 
 export const profileRouter = createTRPCRouter({});
