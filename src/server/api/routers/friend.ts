ee646d7f086f4167c6985d5183cc4dabc5096bbe
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-15 11:12:02 +0700

    feat: remove friendship

--- src/server/api/routers/friend.ts
+++ src/server/api/routers/friend.ts
@@ -1,3 +1,61 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
-export const friendRouter = createTRPCRouter({});
+export const friendRouter = createTRPCRouter({
+  removeFriend: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string().uuid(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const friendship = await ctx.prisma.friendship.findFirst({
+        where: {
+          OR: [
+            {
+              userInitiatorId: ctx.session.user.id,
+              userReceiverId: input.userId,
+            },
+            {
+              userReceiverId: ctx.session.user.id,
+              userInitiatorId: input.userId,
+            },
+          ],
+        },
+      });
+
+      if (friendship !== null) {
+        if (friendship.accepted) {
+          await ctx.prisma.profile.updateMany({
+            where: {
+              userId: {
+                in: [ctx.session.user.id, input.userId],
+              },
+            },
+            data: {
+              friendCount: {
+                decrement: 1,
+              },
+            },
+          });
+        }
+
+        await ctx.prisma.friendship.delete({
+          where: {
+            userInitiatorId_userReceiverId: {
+              userReceiverId: friendship.userReceiverId,
+              userInitiatorId: friendship.userInitiatorId,
+            },
+          },
+        });
+
+        return true;
+      }
+
+      throw new TRPCError({
+        message: "User is not a friend or in request for friendship",
+        code: "BAD_REQUEST",
+      });
+    }),
+});
