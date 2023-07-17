367168e4ab3be868527111a65b7d247819ec7b17
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-17 19:51:28 +0700

    update friendship status

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,7 +1,15 @@
 import { type Profile } from "@prisma/client";
 import { createTRPCRouter } from "~/server/api/trpc";
 
-export type FRIENDSHIP_STATUS = "NOT_FRIEND" | "FRIEND" | "REQUESTED";
+/* Status pertemanan
+ * REQUESTING_FRIENDSHIP: ketika user A (yang login saat ini) mengirim permintaan pertemanan kepada B, tetapi belum diterima
+ * WAITING_FOR_ACCEPTANCE: ketika user B mengirim permintaan kepada user A (yang login saat ini), tetapi belum diterima
+ * */
+export type FRIENDSHIP_STATUS =
+  | "NOT_FRIEND"
+  | "FRIEND"
+  | "REQUESTING_FRIENDSHIP"
+  | "WAITING_FOR_ACCEPTANCE";
 
 export type SelfProfile = {
   id: string;
