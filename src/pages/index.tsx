1b6f3eb7aab9eb65aa81f8b8dc466b7aa33b102e
Gulilil <juan.csantoso@gmail.com>
2023-07-19 10:31:23 +0700

    test: design system feat test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { type NextPage } from "next";
 import Layout from "~/layout";
-import { Container } from "@chakra-ui/react";
+import { Container, theme } from "@chakra-ui/react";
+import { Theme } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
-      <Container>Hello world</Container>
+      <Container>Hello world </Container>
     </Layout>
   );
 };
