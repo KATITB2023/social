dc3e8edf0366536f99470eb1b8d27f2f9d3d3bf7
Gulilil <juan.csantoso@gmail.com>
2023-07-19 10:35:43 +0700

    style: delete unnecsessary codeline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { type NextPage } from "next";
 import Layout from "~/layout";
 import { Container, theme } from "@chakra-ui/react";
-import { Theme } from "@chakra-ui/react";
-import { color } from "framer-motion";
 
 const Home: NextPage = () => {
   return (
