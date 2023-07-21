e0f7476779f601af6e45144a20cf1bdcb160d029
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2023-07-21 21:10:55 +0700

    feat: add get user profile

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,7 +1,36 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { SelfProfile } from "~/server/types/user-profile";
 
 export const profileRouter = createTRPCRouter({
   hello: publicProcedure.query(() => {
     return "hello world";
+  }),
+
+  getUserProfile: protectedProcedure.query(async ({ ctx }) => {
+    const sessionUser = ctx.session.user;
+
+    const currUser = await ctx.prisma.user.findUnique({
+      select: {
+        profile: true,
+        nim: true,
+        id: true,
+      },
+      where: {
+        nim: sessionUser.nim,
+      },
+    });
+
+    if (currUser?.profile) {
+      const { userId, updatedAt, ...profileDetails } = currUser.profile;
+      return {
+        id: currUser.id,
+        nim: currUser.nim,
+        ...profileDetails,
+      } as SelfProfile;
+    }
   }),
 });
