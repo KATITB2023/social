5cc43f7efaeed1c4aef1241fe238dbf75c0bed51
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-21 14:19:07 +0700

    fix merge

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,9 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { date, z } from "zod";
-import { TRPCError } from "@trpc/server";
 import { compareHash, generateHash, generateResetToken } from "~/utils/auth";
-import { generateResetToken } from "~/utils/auth";
 import { TRPCError } from "@trpc/server";
 
 export const authRouter = createTRPCRouter({
@@ -47,79 +44,77 @@ export const authRouter = createTRPCRouter({
 
       return `https://BASE_URL/reset-password/${profile.userId}/${resetToken}`;
     }),
-  hello: publicProcedure.query(() => {
-    return "hello world";
-  }),
-
   resetPassword: publicProcedure
-    .input(z.object({ userId: z.string().uuid(),
-                      token: z.string(),
-                      newPassword: z.string() }))
-    .query(async ({ctx, input}) => {
-
+    .input(
+      z.object({
+        userId: z.string().uuid(),
+        token: z.string(),
+        newPassword: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
       // Ambil token dari userId
       const getToken = await ctx.prisma.resetToken.findFirst({
-        select:{
+        select: {
           token: true,
           createdAt: true,
-          expireTime: true
+          expireTime: true,
         },
-        where:{
-          userId: input.userId
-        }
-      })
+        where: {
+          userId: input.userId,
+        },
+      });
 
       // Error Message (User tidak ada di table "Reset Token")
-      if (!getToken){
-
+      if (!getToken) {
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: "User Tidak Ditemukan!"
-        })
+          message: "User Tidak Ditemukan!",
+        });
+      } else {
+        // User ditemukan
 
-      } else { // User ditemukan
-
-        const createdAt = getToken.createdAt
-        const expiredTime = (createdAt.getTime() / 1000) + getToken.expireTime
-        const currentTime = new Date().getTime() / 1000
+        const createdAt = getToken.createdAt;
+        const expiredTime = createdAt.getTime() / 1000 + getToken.expireTime;
+        const currentTime = new Date().getTime() / 1000;
 
         // Error Message (Token Expired)
         if (expiredTime < currentTime) {
           throw new TRPCError({
             code: "BAD_REQUEST",
-            message: "Token Expired!"
-          })
+            message: "Token Expired!",
+          });
         }
 
         // Cek Token Valid atau Tidak Valid
-        if (!await compareHash(input.token, getToken.token)){
+        if (!(await compareHash(input.token, getToken.token))) {
           // Error Message (Token Tidak Valid)
           throw new TRPCError({
             code: "BAD_REQUEST",
-            message: "Token Tidak Valid!"
-          })
-
-        } else { // Token Valid
+            message: "Token Tidak Valid!",
+          });
+        } else {
+          // Token Valid
 
           // Change Password
           await ctx.prisma.user.update({
-            where:{
-              id: input.userId
+            where: {
+              id: input.userId,
             },
-            data :{
-              passwordHash: await generateHash(input.newPassword)
-            }
-          })
+            data: {
+              passwordHash: await generateHash(input.newPassword),
+            },
+          });
 
           // Hapus Token dari table "Reset Token"
           await ctx.prisma.resetToken.delete({
-            where:{
-              userId: input.userId
-            }
-          })
+            where: {
+              userId: input.userId,
+            },
+          });
 
           // Message Berhasil Ubah Password
-          return "Password telah diubah!"
+          return "Password telah diubah!";
         }
       }
     }),
