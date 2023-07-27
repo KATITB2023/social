3cc43cce3c2c4b0757eadf8ab20b17d58584cecd
Gulilil <juan.csantoso@gmail.com>
2023-07-27 17:36:33 +0700

    feat: add webcame component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { type NextPage } from "next";
 import Layout from "~/layout";
 import { Container, Heading, theme, Text } from "@chakra-ui/react";
+import { SelectImage } from "~/components/SelectImage";
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
       <Text size="B1">B1</Text>
       <Text size="B5">B5</Text>
       <Text size="A">Additional</Text>
+      <SelectImage/>
     </Layout>
   );
 };
