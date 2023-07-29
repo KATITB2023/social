047e0977f913221195b7eb18d6357289f9668f0c
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-07-29 11:10:16 +0700

    move feed type to types

--- src/server/api/routers/feeds.ts
+++ src/server/api/routers/feeds.ts
@@ -1,15 +1,4 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { type Feed as BaseFeed } from "@prisma/client";
-
-export interface Feed extends BaseFeed {
-  reactions: {
-    [k: string]: {
-      count: number;
-      reacted: boolean;
-    };
-  };
-  read: boolean;
-}
 
 export const feedRouter = createTRPCRouter({
   getFeeds: protectedProcedure.query(() => "hello"),
