08b8f194a561752edec3d7d46e03dd7daf8a9aee
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-08-03 11:06:59 +0700

    fead: make profile image nullable

--- src/server/api/routers/anon-message.ts
+++ src/server/api/routers/anon-message.ts
@@ -4,88 +4,88 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { type AnonChatHeader } from "~/server/types/message";
 
 export const anonymousMessageRouter = createTRPCRouter({
-  chatHeader: protectedProcedure.input(
-    z.object(
-      {
+  chatHeader: protectedProcedure
+    .input(
+      z.object({
         limit: z.number().min(5).max(40).default(20),
         page: z.number().min(1).default(1),
-      }
+      })
     )
-  )
-    .query(
-      async ({ ctx, input }): Promise<AnonChatHeader[]> => {
-        const chatHeaders = await ctx.prisma.userMatch.findMany({
-          where: {
-            AND: [
-              {
-                OR: [
-                  {
-                    firstUserId: ctx.session.user.id,
-                  },
-                  {
-                    secondUserId: ctx.session.user.id,
-                  },
-                ],
-              },
-              {
-                endedAt: {
-                  not: null,
-                }
-              },
-            ],
-          },
-          include: {
-            firstUser: {
-              select: {
-                id: true,
-                profile: {
-                  select: {
-                    name: true,
-                    image: true,
-                  }
-                }
-              }
+    .query(async ({ ctx, input }): Promise<AnonChatHeader[]> => {
+      const chatHeaders = await ctx.prisma.userMatch.findMany({
+        where: {
+          AND: [
+            {
+              OR: [
+                {
+                  firstUserId: ctx.session.user.id,
+                },
+                {
+                  secondUserId: ctx.session.user.id,
+                },
+              ],
             },
-            secondUser: {
-              select: {
-                id: true,
-                profile: {
-                  select: {
-                    name: true,
-                    image: true,
-                  }
-                }
+            {
+              endedAt: {
+                not: null,
+              },
+            },
+          ],
+        },
+        include: {
+          firstUser: {
+            select: {
+              id: true,
+              profile: {
+                select: {
+                  name: true,
+                  image: true,
+                },
               },
             },
           },
-          orderBy: {
-            endedAt: "desc",
+          secondUser: {
+            select: {
+              id: true,
+              profile: {
+                select: {
+                  name: true,
+                  image: true,
+                },
+              },
+            },
           },
-          skip: input.limit * (input.page - 1),
-          take: input.limit,
-        });
+        },
+        orderBy: {
+          endedAt: "desc",
+        },
+        skip: input.limit * (input.page - 1),
+        take: input.limit,
+      });
 
-        return chatHeaders.map((chatHeader) => {
-          
-          const otherUser = chatHeader.firstUserId === ctx.session.user.id ? chatHeader.secondUser : chatHeader.firstUser;
-          if (!otherUser.profile) {
-            throw new TRPCError({
-              code: "INTERNAL_SERVER_ERROR",
-              message: "Other user profile not found",
-            });
-          }
+      return chatHeaders.map((chatHeader) => {
+        const otherUser =
+          chatHeader.firstUserId === ctx.session.user.id
+            ? chatHeader.secondUser
+            : chatHeader.firstUser;
+        if (!otherUser.profile) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Other user profile not found",
+          });
+        }
 
-          return {
-            user: {
-              id: otherUser.id,
-              name: !chatHeader.isRevealed ? "Anonymous" : otherUser.profile.name,
-              profileImage: !chatHeader.isRevealed ? "Anonymous" : (otherUser.profile.image ? otherUser.profile.image : "profileImage Not Found"),
-            }
-          }
-        });
-
-      }
-    ),
+        return {
+          user: {
+            id: otherUser.id,
+            name: !chatHeader.isRevealed ? "Anonymous" : otherUser.profile.name,
+            profileImage: !chatHeader.isRevealed
+              ? null
+              : otherUser.profile.image,
+          },
+        };
+      });
+    }),
 
   infinite: protectedProcedure
     .input(
@@ -107,11 +107,11 @@ export const anonymousMessageRouter = createTRPCRouter({
         },
         cursor: input.cursor
           ? {
-            createdAt_id: {
-              createdAt: input.cursor.date,
-              id: input.cursor.id,
-            },
-          }
+              createdAt_id: {
+                createdAt: input.cursor.date,
+                id: input.cursor.id,
+              },
+            }
           : undefined,
         take: input.take + 1,
         skip: 0,
