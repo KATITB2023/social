d3d67f8ab4e874451dfdaeeb72f046bf15087cad
putrafurqan <furqanriansyah@gmail.com>
2023-08-04 23:17:00 +0700

    refactor: query optimization

--- src/server/api/routers/feeds.ts
+++ src/server/api/routers/feeds.ts
@@ -14,7 +14,6 @@ export const feedRouter = createTRPCRouter({
     )
 
     .query(async ({ ctx, input }) => {
-
       // Query feeds
       const feeds = await ctx.prisma.feed.findMany({
         take: input.limit,
@@ -29,66 +28,73 @@ export const feedRouter = createTRPCRouter({
           createdAt: true,
         },
       });
-
-      // Iterate over every feed
-      const feedList = await Promise.all(feeds.map(async (feed) => {
-
-        // Group feed reactions
-        const groupedReactions = await ctx.prisma.feedReaction.groupBy({
-          by: ["reaction"],
-          where: {
-            feedId: feed.id,
+    
+      const feedIds = feeds.map(feed => feed.id);
+    
+      // Group feed reactions
+      const groupedReactions = await ctx.prisma.feedReaction.groupBy({
+        by: ["feedId", "reaction"],
+        where: {
+          feedId: {
+            in: feedIds
+          }
+        },
+        _count: {
+          reaction: true,
+        },
+      });
+    
+      // Get user reactions
+      const userReactions = await ctx.prisma.feedReaction.findMany({
+        where: {
+          feedId: {
+            in: feedIds
           },
-          _count: {
-            reaction: true,
-          },  
-        });
-
-        // Check if user has reacted to this feed
-        const userReaction = await ctx.prisma.feedReaction.findFirst({
-          where: {
-            feedId: feed.id,
-            userId: ctx.session.user.id,
+          userId: ctx.session.user.id,
+        },
+        select: {
+          feedId: true,
+          reaction: true,
+        },
+      });
+    
+      // Check if user has read these feeds
+      const userReadFeeds = await ctx.prisma.feedRead.findMany({
+        where: {
+          feedId: {
+            in: feedIds
           },
-          select: {
-            reaction: true,
-          },
-        });
-
-        // Transform groupedReactions and userReaction into reactions object
-        const reactions: extendFeed["reactions"] = {};      
-        groupedReactions.forEach((group) => {
+          userId: ctx.session.user.id,
+        },
+        select: {
+          feedId: true
+        }
+      });
+    
+      // Transform into reactions object
+      const feedList = feeds.map(feed => {
+  
+        const reactions: extendFeed["reactions"] = {};
+        
+        groupedReactions.filter(group => group.feedId === feed.id).forEach(group => {
+  
           reactions[group.reaction] = {
             count: group._count.reaction,
-            reacted: userReaction?.reaction === group.reaction || false,
+            reacted: userReactions.some(userReaction => userReaction.feedId === feed.id && userReaction.reaction === group.reaction) || false,
           };
-        }); 
-
-        // Check if user has read this feed
-        let read: extendFeed["read"];
-        const isRead = await ctx.prisma.feedRead.findFirst({
-          where: {
-            feedId: feed.id,
-            userId: ctx.session.user.id,
-          },
+          
         });
-
-        if (isRead) {
-          read = true; 
-        } else {
-          read = false;
-        }
-
-
-        return { 
-          ...feed, 
+    
+        let read: extendFeed["read"] = userReadFeeds.some(userReadFeed => userReadFeed.feedId === feed.id);
+    
+        return {
+          ...feed,
           reactions,
           read,
         };
-    }));
-
-    return feedList
-
+      });
+    
+      return feedList;
     }),
   react: protectedProcedure.mutation(() => "hello"),
 });
