8cae0855800c40acc82fb60a3a4ddb039392ea2b
Valentino Triadi <valent122016@gmail.com>
2023-08-04 19:30:37 +0700

    refactor: add pagination

--- src/server/api/routers/leaderboard.ts
+++ src/server/api/routers/leaderboard.ts
@@ -8,21 +8,30 @@ import { Leaderboard } from "~/server/types/leaderboard";
 
 export const leaderboardRouter = createTRPCRouter({
   getLeaderboard: publicProcedure
-  .input(z.object({ page: z.number().int().gte(0).lte(10).default(0) }))
+  .input(z.object({ page: z.number().int().default(1),
+                    limit: z.number().int().default(20) }))
   .query(async({ctx, input}): Promise<Leaderboard[]> => {
     
     const profileData = await ctx.prisma.profile.findMany({
-      orderBy: [{ point: "desc" }]
+      orderBy: [{ point: "desc" }],
+      take: input.limit,
+      skip: (input.page - 1) * input.limit,
+      select:{
+        userId:true,
+        name:true,
+        image:true,
+        point:true
+      }
     })
     
     let leaderboardData:Leaderboard[] = []
-    let currentRank = 1
+    let currentRank = ((input.page - 1) * 5) + 1
     let totalSamePoint = 0
     let stillSame = false
 
     for (const profile of profileData) {
       // Cek poin sama/tidak dengan point user sebelumnya 
-      if (leaderboardData[0] && leaderboardData[currentRank + totalSamePoint - 2]?.point == profile.point){
+      if (leaderboardData[0] && leaderboardData[(currentRank-((input.page - 1)*5)) + totalSamePoint - 2]?.point == profile.point){
         currentRank--;
         totalSamePoint++
         stillSame = true
