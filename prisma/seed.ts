e30d528e508db9c5cadf1377545c85eeccb27293
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-08-06 19:58:53 +0700

    chore: add feed seed, bump db version

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,17 +6,26 @@ void (async () => {
   let nim = 0;
   const prisma = new PrismaClient();
 
+  // seed user
   for (let i = 0; i < 5; i++) {
-    const user = await prisma.user.create({
-      data: {
+    const user = await prisma.user.upsert({
+      where: {
+        nim: "13521" + String(nim++).padStart(3, "0"),
+      },
+      update: {},
+      create: {
         nim: "13521" + String(nim++).padStart(3, "0"),
         role: i === 0 ? "ADMIN" : i === 1 ? "MENTOR" : "STUDENT",
         passwordHash: await hash("password", 10),
       },
     });
 
-    await prisma.profile.create({
-      data: {
+    await prisma.profile.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {},
+      create: {
         userId: user.id,
         name: faker.person.fullName(),
         pin: faker.string.numeric({ length: 6 }), // just pray that this will return unique pin
@@ -25,6 +34,15 @@ void (async () => {
         gender: faker.helpers.arrayElement(["MALE", "FEMALE"]),
         email: faker.internet.email(),
         bio: faker.lorem.words(10),
+      },
+    });
+  }
+
+  for (let i = 0; i < 15; i++) {
+    await prisma.feed.create({
+      data: {
+        text: faker.lorem.lines(5),
+        attachmentUrl: null,
       },
     });
   }
