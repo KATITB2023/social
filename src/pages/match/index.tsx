336a3a34f5423049700b17b88c8c4f98ba4e7123
nathaniacalista01 <nathania.calista01@gmail.com>
2023-08-06 23:03:42 +0700

    feat : connect ke BE

--- src/pages/match/index.tsx
+++ src/pages/match/index.tsx
@@ -20,61 +20,60 @@ import Footer from "~/components/newchat/Footer";
 import Eclipse from "~/components/newchat/Eclipse";
 import FirstNewChatButton from "~/components/newchat/Button";
 import QuestionBox from "~/components/newchat/Box";
-import { FirstQuestion, SecondQuestion, ThirdQuestion } from "~/components/newchat/Questions";
-import Loading from "~/components/newchat/Loading";
+import {
+  FirstQuestion,
+  SecondQuestion,
+  ThirdQuestion,
+} from "~/components/newchat/Questions";
 
 const Match: NextPage = () => {
   const [questionPage, setQuestionPage] = useState(1);
   const router = useRouter();
-  // useSession({ required: true });
+  useSession({ required: true });
 
-  // const [needQueue, setNeedQueue] = useState<boolean>(false);
-  // const [queued, setQueued] = useState<boolean>(false);
-  // const [foundMatch, setFoundMatch] = useState<boolean>(false);
+  const [needQueue, setNeedQueue] = useState<boolean>(false);
+  const [queued, setQueued] = useState<boolean>(false);
+  const [foundMatch, setFoundMatch] = useState<boolean>(false);
+  const checkEmit = useEmit("checkMatch", {
+    onSuccess: (data) => {
+      if (data !== null) {
+        void router.push(`/match/room`);
+      } else {
+        setNeedQueue(true);
+      }
+    },
+  });
+  const queueEmit = useEmit("findMatch");
+  const cancelEmit = useEmit("cancelMatch");
+  useEffect(() => {
+    checkEmit.mutate({});
 
-  // const checkEmit = useEmit("checkMatch", {
-  //   onSuccess: (data) => {
-  //     if (data !== null) {
-  //       void router.push(`/match/room`);
-  //     } else {
-  //       setNeedQueue(true);
-  //     }
-  //   },
-  // });
+    return () => {
+      if (queued) {
+        console.log("cancelling emit");
+        cancelEmit.mutate({});
+      }
+    };
+  }, []);
 
-  // const queueEmit = useEmit("findMatch");
-  // const cancelEmit = useEmit("cancelMatch");
-
-  // useEffect(() => {
-  //   checkEmit.mutate({});
-
-  //   return () => {
-  //     if (queued) {
-  //       console.log("cancelling emit");
-  //       cancelEmit.mutate({});
-  //     }
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   if (!queued && needQueue && !foundMatch) {
-  //     console.log("request queue");
-  //     queueEmit.mutate({
-  //       isAnonymous: true,
-  //       isFindingFriend: true,
-  //       topic: ChatTopic.ITB,
-  //     });
-  //     setQueued(true);
-  //   }
-  // }, [queued, needQueue, queueEmit, foundMatch]);
-
-  // useSubscription("match", (match) => {
-  //   setFoundMatch(true);
-  //   void router.push(`/match/room`);
-  // });
-  const handlePageChange = (page : number)=>{
-    setQuestionPage(page)
-  }
+  const findMatch = () => {
+    if (!queued && needQueue && !foundMatch) {
+      console.log("request queue");
+      queueEmit.mutate({
+        isAnonymous: true,
+        isFindingFriend: true,
+        topic: ChatTopic.ITB,
+      });
+      setQueued(true);
+    }
+  };
+  useSubscription("match", (match) => {
+    setFoundMatch(true);
+    void router.push(`/match/room`);
+  });
+  const handlePageChange = (page: number) => {
+    setQuestionPage(page);
+  };
   return (
     <Box position={"relative"} minH={"100vh"} height="100%">
       <Image
@@ -127,9 +126,18 @@ const Match: NextPage = () => {
       />
       <VStack height="95vh" width="100%" justifyContent={"center"} zIndex={4}>
         <Navbar />
-         {questionPage === 1 && <FirstQuestion handlePageChange={handlePageChange} />}
-         {questionPage === 2 && <SecondQuestion handlePageChange={handlePageChange} />}
-         {questionPage === 3 && <ThirdQuestion handlePageChange={handlePageChange} />}
+        {questionPage === 1 && (
+          <FirstQuestion handlePageChange={handlePageChange} />
+        )}
+        {questionPage === 2 && (
+          <SecondQuestion handlePageChange={handlePageChange} />
+        )}
+        {questionPage === 3 && (
+          <ThirdQuestion
+            handlePageChange={handlePageChange}
+            findMatch={findMatch}
+          />
+        )}
         <Footer />
       </VStack>
     </Box>
