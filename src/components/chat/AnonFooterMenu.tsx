78e54d62c4eaff373b948117432d7996e570e16d
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-08-07 23:00:26 +0700

    feat: add askReveal button

--- src/components/chat/AnonFooterMenu.tsx
+++ src/components/chat/AnonFooterMenu.tsx
@@ -4,15 +4,23 @@ import { Button } from "@chakra-ui/react";
 
 const AnonFooterMenu = () => {
   const endMatch = useEmit("endMatch");
+  const askReveal = useEmit("askReveal");
 
   const handleEndMatch = () => {
     endMatch.mutate(undefined);
+  };
+
+  const handleAskReveal = () => {
+    askReveal.mutate({ agree: true });
   };
 
   return (
     <>
       <Button colorScheme="teal" size="sm" onClick={handleEndMatch}>
         End Match
+      </Button>
+      <Button colorScheme="teal" size="sm" onClick={handleAskReveal}>
+        Ask Reveal
       </Button>
     </>
   );
