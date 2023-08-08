54b30fa0d22f1e2c768c6f2c6bbdccf71bcbff83
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-08-08 12:43:34 +0700

    Trying to change configs again..

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.cjs");
+// Skipping env validation for prod
+// await import("./src/env.cjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
