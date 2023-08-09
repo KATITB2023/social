f2fb645d21e02cd6bc4c6a3d9d5c2a7efb2fbfe8
mrsyaban <mrsyaban99@gmail.com>
2023-08-10 04:58:59 +0700

    feat: integrasi

--- src/pages/attendance-list-page.tsx
+++ src/pages/attendance-list-page.tsx
@@ -10,40 +10,44 @@ import {
 } from "@chakra-ui/react";
 import Navbar from "~/components/Navbar";
 
-import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
-import { stat } from "fs";
+import {
+  type AttendanceEvent,
+  type AttendanceRecord,
+  Status,
+} from "@prisma/client";
+import { useState } from "react";
 
-const eventList = {
-  "Day 1": {
-    "1": {
-      title: "Sesi 2 (Talkshow)",
-      startTime: "17.50",
-      endTime: "18.10",
-      status: "tandai hadir",
-    },
-    "2": {
-      title: "Sesi 2 (Talkshow)",
-      startTime: "17.50",
-      endTime: "18.10",
-      status: "tandai hadir",
-    },
-  },
-  "Day 2": {
-    "1": {
-      title: "Sesi 2 (Talkshow)",
-      startTime: "17.50",
-      endTime: "18.10",
-      status: "tandai hadir",
-    },
-    "2": {
-      title: "Sesi 2 (Talkshow)",
-      startTime: "17.50",
-      endTime: "18.10",
-      status: "tandai hadir",
-    },
-  },
-};
+// const eventList = {
+//   "Day 1": {
+//     "1": {
+//       title: "Sesi 2 (Talkshow)",
+//       startTime: "17.50",
+//       endTime: "18.10",
+//       status: "tandai hadir",
+//     },
+//     "2": {
+//       title: "Sesi 2 (Talkshow)",
+//       startTime: "17.50",
+//       endTime: "18.10",
+//       status: "tandai hadir",
+//     },
+//   },
+//   "Day 2": {
+//     "1": {
+//       title: "Sesi 2 (Talkshow)",
+//       startTime: "17.50",
+//       endTime: "18.10",
+//       status: "tandai hadir",
+//     },
+//     "2": {
+//       title: "Sesi 2 (Talkshow)",
+//       startTime: "17.50",
+//       endTime: "18.10",
+//       status: "tandai hadir",
+//     },
+//   },
+// };
 
 type childrenOnlyProps = {
   children: string | JSX.Element | JSX.Element[];
@@ -141,53 +145,127 @@ const BackgroundAndNavbar = ({ children }: childrenOnlyProps) => {
   );
 };
 
-interface EventData {
-  title: string;
-  startTime: string;
-  endTime: string;
-  status: string;
-}
+const StatusButton = ({ status }: { status: Status }) => {
+  const [loading, setLoading] = useState(false);
+  const absenMutation = api.absensi.submitAbsensi.useMutation();
+  
+  const handleAttend = async (eventId) => {
+    setLoading(true);
+    try {
+      const result = await absenMutation.mutateAsync({
+        eventId
+      });
+    }
+    setLoading(false);
+  }
 
-interface EventCardProps {
-  eventData: EventData;
-}
-
-interface StatButtonProps {
-  status: string;
-}
-
-const StatusButton = (props: StatButtonProps) => {
-  const { status } = props;
-  if (status == "tandai hadir") {
+  if (status === Status.TIDAK_HADIR) {
     return (
-      <Button bg="yellow.5" borderRadius="12px" py="4px" px="16px" size="xs">
-        <Text color="purple.2" size="A">
-          {status}
-        </Text>
-      </Button>
-    );
-  } else if (status == "hadir") {
-    return (
-      <Button border="1px" borderColor="green.2" bg="linear-gradient(0deg, rgba(114, 216, 186, 0.50) 0%, rgba(114, 216, 186, 0.50) 100%), #FFF" borderRadius="12px" py="4px" px="16px" size="xs">
-        <Text color="green.2" size="A">
-          {status}
-        </Text>
-      </Button>
-    );
-  } else {
-    return (
-      <Button  border="1px" borderColor="#E8553E" bg="linear-gradient(0deg, rgba(232, 85, 62, 0.50) 0%, rgba(232, 85, 62, 0.50) 100%), #FFF" borderRadius="12px" py="4px" px="16px" size="xs">
+      <Button
+        border="1px"
+        borderColor="#E8553E"
+        bg="linear-gradient(0deg, rgba(232, 85, 62, 0.50) 0%, rgba(232, 85, 62, 0.50) 100%), #FFF"
+        borderRadius="12px"
+        py="4px"
+        px="16px"
+        size="xs"
+      >
         <Text color="#E8553E" size="A">
-          {status}
+          tidak hadir
+        </Text>
+      </Button>
+    );
+  } else if (status === Status.HADIR) {
+    return (
+      <Button
+        border="1px"
+        borderColor="green.2"
+        bg="linear-gradient(0deg, rgba(114, 216, 186, 0.50) 0%, rgba(114, 216, 186, 0.50) 100%), #FFF"
+        borderRadius="12px"
+        py="4px"
+        px="16px"
+        size="xs"
+      >
+        <Text color="green.2" size="A">
+          hadir
+        </Text>
+      </Button>
+    );
+  } else if (status === Status.IZIN_DITERIMA) {
+    return (
+      <Button
+        border="1px"
+        borderColor="green.2"
+        bg="linear-gradient(0deg, rgba(114, 216, 186, 0.50) 0%, rgba(114, 216, 186, 0.50) 100%), #FFF"
+        borderRadius="12px"
+        py="4px"
+        px="16px"
+        size="xs"
+      >
+        <Text color="green.2" size="A">
+          izin diterima
         </Text>
       </Button>
     )
+  } else if (status === Status.IZIN_PENDING) {
+    <Button
+      border="1px"
+      borderColor="green.2"
+      bg="linear-gradient(0deg, rgba(114, 216, 186, 0.50) 0%, rgba(114, 216, 186, 0.50) 100%), #FFF"
+      borderRadius="12px"
+      py="4px"
+      px="16px"
+      size="xs"
+    >
+      <Text color="green.2" size="A">
+        izin pending
+      </Text>
+    </Button>
+  } else if (status === Status.IZIN_DITOLAK) {
+    <Button
+      border="1px"
+      borderColor="green.2"
+      bg="linear-gradient(0deg, rgba(114, 216, 186, 0.50) 0%, rgba(114, 216, 186, 0.50) 100%), #FFF"
+      borderRadius="12px"
+      py="4px"
+      px="16px"
+      size="xs"
+    >
+      <Text color="green.2" size="A">
+        izin ditolak
+      </Text>
+    </Button>
+  } else {
+    return (
+      <Button bg="yellow.5" borderRadius="12px" py="4px" px="16px" size="xs" onClick={handleAttend}>
+        <Text color="purple.2" size="A">
+          tandai hadir
+        </Text>
+      </Button>
+    );
   }
 };
 
-const EventCard = (props: EventCardProps) => {
-  const { eventData } = props;
+const getTwoTime = (startDate: Date, endDate: Date) => {
+  const startTime = startDate.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const endTime = endDate.toLocaleTimeString("id-ID", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
+  return `${startTime} - ${endTime}`;
+};
+
+const EventCard = ({
+  event,
+  status,
+}: {
+  event: AttendanceEvent;
+  status: Status;
+}) => {
   return (
     <Flex
       flexDir="row"
@@ -198,27 +276,73 @@ const EventCard = (props: EventCardProps) => {
     >
       <Flex flexDir="column" gap="8px">
         <Heading size="SH5" color="yellow.4">
-          {eventData.title}
+          {event.title}
         </Heading>
         <Flex alignItems="center">
           <Text size="B5" fontWeight="bold">
             Waktu
           </Text>
           <Text size="B5">
-            &nbsp;: {eventData.startTime} - {eventData.endTime}
+            &nbsp;: {getTwoTime(event.startTime, event.endTime)}
           </Text>
         </Flex>
       </Flex>
       <Spacer />
-      <StatusButton status="hadir"/>
+      <StatusButton status={status} />
     </Flex>
   );
 };
 
+interface AbsenStatus {
+  event: AttendanceEvent;
+  record: AttendanceRecord | null;
+  status: Status | null;
+}
+
+interface EventsDay {
+  day: string;
+  events: AbsenStatus[];
+}
+
+const isThere = ({eventsDay, dayId} : {eventsDay: EventsDay[]; dayId:string;}) => {
+  eventsDay.forEach(({day, events}) => {
+    if (dayId === day) {
+      return true;
+    } 
+  }) 
+  return false;
+}
+
 const AttendListPage = () => {
-  const { data: session } = useSession();
   const eventQuery = api.absensi.viewAbsensi.useQuery();
-  const eventListt = eventQuery?.data;
+  const eventList = eventQuery?.data;
+
+  const eventsByDay: EventsDay[]= []
+
+
+  if (eventList) {
+    eventList.forEach(({ event, record, status }) => {
+      const dayId = event.dayId;
+
+      // Create a new AbsenStatus object
+      const absenStatus: AbsenStatus = {
+        event,
+        record,
+        status,
+      };
+
+      // If the day is not already in the eventsByDay array, add it
+      if (isThere(eventsByDay)) {
+        eventsByDay.push({
+          day: dayId,
+          events: [absenStatus],
+        });
+      } else {
+        // If the day is already in the eventsByDay array, push the AbsenStatus to its events
+        eventsByDay[dayIndex].events.push(absenStatus);
+      }
+    });
+  }
 
   return (
     <BackgroundAndNavbar>
@@ -227,6 +351,21 @@ const AttendListPage = () => {
           <Heading size="H4" color="yellow.400" mx="auto" mt="128px">
             ATTENDANCE LIST
           </Heading>
+          <Flex flexDir="column">
+            {eventListt?.map((item) => (
+              <Flex key={item.event.id} flexDir="column" gap="20px" mt="44px">
+                <Heading size="SH4">{item.event.dayId}</Heading>
+                {Object.entries(events).map(([id, event]) => (
+                  <EventCard key={id} eventData={event} />
+                ))}
+              </Flex>
+              // <Flex key={item.event.id} justifyContent="space-between" alignItems="center" p={2} border="1px solid gray">
+              //   <Text>{item.event.title}</Text>
+              //   <Text>Status: {item.status}</Text>
+              //   {/* You can add more information here */}
+              // </Flex>
+            ))}
+          </Flex>
           {Object.entries(eventList).map(([day, events]) => (
             <Flex key={day} flexDir="column" gap="20px" mt="44px">
               <Heading size="SH4">{day}</Heading>
