6502b6d749c08db899c23ab7478b6ff61d0c9ad8
akmal <akmalramadannn@gmail.com>
2023-08-09 18:52:10 +0700

    revision

---
+++ src/components/PopupChat/YayTemenmu.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import Popup from "~/components/PopupChat/PopUp"
+
+const BerhasilRequest: React.FC = () => {
+  return (
+    <div>
+      <Popup content1="Yay" content2='temenmu' content3='udah reveal' content4='profil nih!'/>
+    </div>
+  )
+}
+
+export default BerhasilRequest
