799975a1c5a8d4062287d1a8f81c59ec677fd524
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-08-10 22:05:03 +0700

    fix feed merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,12 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import React, { useEffect, useRef } from "react";
-import Navbar from "~/components/Navbar";
 import { api } from "~/utils/api";
 import { useSession } from "next-auth/react";
 import Feed from "~/components/feeds/Feed";
 import { useInView } from "framer-motion";
 import Layout from "~/layout";
-import Footer from "~/components/Footer";
+import BackgroundAndNavbar from "~/components/BackgroundAndNavbar";
 
-type childrenOnlyProps = {
-  children: string | JSX.Element | JSX.Element[];
-};
-
-interface Feed {
-  date: string;
-  caption: string;
-  link: string;
-}
-
-function BackgroundAndNavbar({ children }: childrenOnlyProps) {
-  return (
-    <Box
-      display={"flex"}
-      position="relative"
-      minHeight="100vh"
-      height="100%"
-      maxW={375}
-    >
-      <Image
-        src="background-feeds.svg"
-        alt="background"
-        height="100%"
-        zIndex="-1"
-        position="absolute"
-        objectFit="cover"
-        minWidth="100%"
-        width="100%"
-      />
-      <Flex flexDirection="column" width={"100%"}>
-        <Navbar currentPage="Feeds" />
-        {children}
-      </Flex>
-    </Box>
-  );
-}
 export default function FeedsPage() {
   useSession({ required: true });
   const bottomRef = useRef<HTMLDivElement | null>(null);
@@ -64,29 +27,29 @@ export default function FeedsPage() {
       fetchNextPage().catch((e) => console.log(e));
     }
   }, [bottomView, hasNextPage, fetchNextPage]);
-  
+
   return (
-<Layout title={"Beranda"}>
-    <BackgroundAndNavbar>
-      <Flex flexDirection={"column"} justifyContent={"center"}>
-        {data?.pages
-          .flatMap((page) => page.data)
-          .map((feed) => {
-            return (
-              <Feed
-                key={feed.id}
-                reactions={feed.reactions}
-                attachmentUrl={feed.attachmentUrl}
-                createdAt={feed.createdAt}
-                text={feed.text}
-                id={feed.id}
-                read = {feed.read}
-              />
-            );
-          })}
-        <div ref={bottomRef}></div>
-      </Flex>
-    </BackgroundAndNavbar>
-</Layout>
+    <Layout title={"Beranda"}>
+      <BackgroundAndNavbar>
+        <Flex flexDirection={"column"} justifyContent={"center"}>
+          {data?.pages
+            .flatMap((page) => page.data)
+            .map((feed) => {
+              return (
+                <Feed
+                  key={feed.id}
+                  reactions={feed.reactions}
+                  attachmentUrl={feed.attachmentUrl}
+                  createdAt={feed.createdAt}
+                  text={feed.text}
+                  id={feed.id}
+                  read={feed.read}
+                />
+              );
+            })}
+          <div ref={bottomRef}></div>
+        </Flex>
+      </BackgroundAndNavbar>
+    </Layout>
   );
 }
