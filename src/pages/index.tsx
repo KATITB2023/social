e32701cd829301ed9e5a8a5cdffdcf7d2f075ee8
Filbert88 <filbertfilbert21@gmail.com>
2023-08-10 22:08:43 +0700

    fix: fixed lint error issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import { type NextPage } from "next";
 import Layout from "~/layout";
-import { Container, Heading, theme, Text, Box, Button, Image } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  theme,
+  Text,
+  Box,
+  Button,
+  Image,
+} from "@chakra-ui/react";
 import Navbar from "~/components/Navbar";
+import Leaderboard from "./leaderboard";
 
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
-      <Navbar currentPage="Back to Home"/>
-      <Container>Hello world </Container>
+      {/* <Navbar currentPage="Back to Home" /> */}
+      {/* <Container>Hello world </Container>
       <Heading size="SH1">SH1</Heading>
       <Heading size="H3">h3</Heading>
       <Heading size="H1">h1</Heading>
@@ -21,7 +30,8 @@ const Home: NextPage = () => {
       <Text>Default Text</Text>
       <Text size="B1">B1</Text>
       <Text size="B5">B5</Text>
-      <Text size="A">Additional</Text>
+      <Text size="A">Additional</Text> */}
+      <Leaderboard />
     </Layout>
   );
 };
