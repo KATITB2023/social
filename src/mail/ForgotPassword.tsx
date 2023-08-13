f593e9cc72a2c80b50e25b2eb55072a60a719caf
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-08-14 04:13:02 +0700

    feat: email styling

--- src/mail/ForgotPassword.tsx
+++ src/mail/ForgotPassword.tsx
@@ -11,6 +11,7 @@ import {
   Text,
   Row,
   Button,
+  Column,
 } from "@react-email/components";
 
 export const ForgotPassword = ({
@@ -24,38 +25,63 @@ export const ForgotPassword = ({
     <Html>
       <Head />
       <Preview>Reset Password</Preview>
-      <Body>
+      <Body style={main}>
         <Container>
-          <Row>
-            <Row>
-              <Img width={340} src={``} />
+          <Section style={content}>
+            <Img
+              width={750}
+              src="https://github.com/KATITB2023/social/assets/88751131/cc5648e3-e8b1-49ea-8159-7788b63dccb2"
+            />
+
+            <Row style={{ ...boxInfos, paddingBottom: "0" }}>
+              <Column>
+                <Text style={paragraph}>
+                  Hai {name},<br />
+                  <br />
+                  Anda menerima surel ini karena kami menerima permintaan untuk
+                  mengubah password akun Anda. Jika Anda tidak mengirim
+                  permintaan untuk mengubah password, Anda dapat mengabaikan
+                  surel ini.
+                  <br />
+                </Text>
+              </Column>
             </Row>
-            <Row>
-              <Heading>RESET PASSWORD</Heading>
+            <Row style={{ ...boxInfos, paddingTop: "0", paddingBottom: "0" }}>
+              <Column style={containerButton} colSpan={2}>
+                <Button style={button} href={resetURL}>
+                  <Text style={{ ...buttonText, textAlign: "center" }}>
+                    Reset Password
+                  </Text>
+                </Button>
+              </Column>
             </Row>
-          </Row>
-          <Section>
-            <Text>
-              Hai {name},<br />
-              <br />
-              Anda menerima surel ini karena kami menerima permintaan untuk
-              reset password akun Anda. Jika Anda tidak mengirim permintaan
-              untuk me-reset password, Anda dapat mengabaikan surel ini.
-              <br />
-            </Text>
-            <Button href={resetURL}>Reset Password</Button>
-            <Text>
-              Jika tombol di atas tidak berfungsi, silakan klik tautan{" "}
-              <Link href={resetURL}>berikut</Link>.
+
+            <Row style={{ ...boxInfos, paddingTop: "0" }}>
+              <Column>
+                <Text style={paragraph}>
+                  Jika tombol di atas tidak berfungsi, silakan klik tautan{" "}
+                  <Link href={resetURL}>berikut</Link>.
+                </Text>
+              </Column>
+            </Row>
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 12,
+                color: "rgb(0,0,0, 0.7)",
+                // fontFamily: "SomarRounded-Regular",
+              }}
+            >
+              © Orientasi Studi Keluarga Mahasiswa (OSKM) ITB 2023 {"  |  "}
+              <Link
+                href="https://oskmitb.com"
+                style={{ color: "rgb(0,0,0, 0.7)" }}
+              >
+                oskmitb.com
+              </Link>
             </Text>
           </Section>
         </Container>
-        <Section>
-          <Text>Surel ini dikirim oleh Tim OSKM ITB.</Text>
-          <Link href="https://oskmitb.com">Website</Link>
-          {" | "}
-          <Link href={""}>Kontak</Link>
-        </Section>
       </Body>
     </Html>
   );
