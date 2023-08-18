339d42349f373202e7915d9985dad81d362b7552
Bana-man <ahmadalbana98@gmail.com>
2023-08-18 16:17:24 +0700

    feat: change 404 backgroud

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,67 @@
-import React from "react";
-import { Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
-import BackgroundAndNavbar from "~/components/BackgroundAndNavbar";
+import React, { PropsWithChildren } from "react";
+import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import Navbar from "~/components/Navbar";
 import Layout from "~/layout";
+import Footer from "~/components/Footer";
+
+const BackgroundAndNavbar = ({ children }: PropsWithChildren) => {
+  return (
+    <Box
+      position="relative"
+      height="1720px"
+      overflow="hidden"
+    >
+      <Image
+        src="bg.png"
+        alt="background"
+        height="100%"
+        zIndex="-2"
+        position="absolute"
+        objectFit="none"
+        width="375px"
+        top="-33px"
+      />
+      <Image 
+        src="komet4.png"
+        position="absolute"
+        zIndex="-1"
+        top="190px"
+      />
+
+      <Image 
+        src="sparkle telanjang 2.png"
+        position="absolute"
+        right="75px"
+        top="340px"
+        zIndex="-1"
+      />
+      
+      <Flex flexDirection="column">
+        <Navbar />
+        {children}
+      </Flex>
+    </Box>
+  );
+};
 
 const NotFound = () => {
   return (
     <Layout title={"Not found"}>
-      <BackgroundAndNavbar bg="/background.png">
+      <BackgroundAndNavbar>
         <Flex
           flexDirection="column"
-          justifyContent="center"
           gap="14px"
           mx="40px"
-          minH={"80vh"}
+          mt="54px"
+          h="1131px"
         >
           <Image
             src="/404.png"
             alt="404"
             height="100%"
             position="relative"
-            objectFit="cover"
             minWidth="100%"
-            width="100%"
+            h="211px"
           />
           <Heading size="H3" alignSelf="center">
             Ups!
@@ -35,13 +75,14 @@ const NotFound = () => {
             marginBottom="8px"
             align="center"
           >
-            Jangan khawatir, Voyagers! Kalian bisa kembali ke{" "}
+            Jangan khawatir, Spacefarers! Kalian bisa kembali ke{" "}
             <Link color="#FE06BE" href="/">
               home
             </Link>{" "}
             atau kunjungi fitur menarik lainnya di bawah ini.
           </Text>
         </Flex>
+        <Footer />
       </BackgroundAndNavbar>
     </Layout>
   );
