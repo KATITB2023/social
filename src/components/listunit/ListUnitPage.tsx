1d0d98c641475561371f762d3a65dadcf86ec13f
Nyoman-Gana <nyomanganadipa@gmail.com>
2023-08-20 23:00:26 +0700

    Made a to do list for my current working task

---
+++ src/components/listunit/ListUnitPage.tsx
@@ -0,0 +1,35 @@
+import BackgroundAndNavbar from "../BackgroundAndNavbar";
+
+/**
+ * to do:
+ *
+ * 1. Make a temporary card component,
+ *
+ * 2. Make a temporary data to be mapped,
+ *
+ * 3. Make the structure:
+ *
+ * 3.1. Make a button '<' to go back to the page before this one.
+ *
+ * 3.2. Make the header for the entire content
+ *
+ * 3.3. Make a search bar component
+ *
+ * 3.4. Map the temporary data to be a list of cards.
+ *
+ * 4. Style the content.
+ *
+ *
+ *
+ * for scalability:
+ *
+ * (coming soon)
+ */
+
+export default function ListUnitPage() {
+  return (
+    <BackgroundAndNavbar bg="background-bsoukmhimp.svg">
+      ...
+    </BackgroundAndNavbar>
+  );
+}
---
+++ src/pages/listunit.tsx
@@ -0,0 +1,5 @@
+import ListUnitPage from "~/components/listunit/ListUnitPage";
+
+export default function ListUnit() {
+  return <ListUnitPage></ListUnitPage>;
+}
