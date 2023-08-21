25cff5c45f5e877e4f67795602e2a335cb4bea92
Nyoman-Gana <nyomanganadipa@gmail.com>
2023-08-21 19:28:17 +0700

    Finish make a temporary card

---
+++ src/components/listunit/TemporaryCard.tsx
@@ -0,0 +1,10 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+export default function Card() {
+  return (
+    <Box bgColor={"navy.1"} width="150px" height="200px">
+      <Heading>Some content</Heading>
+      <Button></Button>
+    </Box>
+  );
+}
