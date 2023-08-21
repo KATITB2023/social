3803311bca0cded32a182d861e3ccefb7eadca14
Nyoman-Gana <nyomanganadipa@gmail.com>
2023-08-21 21:12:28 +0700

    Finish riwayat unit, list bso, list unit

---
+++ src/components/listpage/ListBSOPage.tsx
@@ -0,0 +1,10 @@
+import BackgroundAndNavbar from "../BackgroundAndNavbar";
+import ListPage from "./ListPage";
+
+export default function ListBSOPage() {
+  return (
+    <BackgroundAndNavbar bg="/background-bsoukmhimp.svg">
+      <ListPage title="BSO" additionTitle="(BADAN SEMI OTONOM)" />
+    </BackgroundAndNavbar>
+  );
+}
