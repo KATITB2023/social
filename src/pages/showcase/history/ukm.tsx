fa6fd0d549a42f8e57edf7af691b4fe6a55ef877
Gulilil <juan.csantoso@gmail.com>
2023-08-22 23:14:52 +0700

    fix: showcase history route

---
+++ src/pages/showcase/history/ukm.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Wrap } from "@chakra-ui/react";
+import { ViewCard } from "~/components/showcase/ViewCard";
+import Layout from "~/layout";
+import Footer from "~/components/Footer";
+import BackgroundAndNavbar from "~/components/BackgroundAndNavbar";
+
+export default function HistoryUKMPage() {
+  return (
+    <Layout title="Riwayat Pengunjungan UKM">
+      <BackgroundAndNavbar bg="/background.png">
+        <Wrap justify={"space-evenly"}>
+          <ViewCard
+            image="/background.png"
+            title="Ini Nama Organisasi"
+            route={"/"}
+          />
+          <ViewCard
+            image="/background.png"
+            title="Ini Nama Organisasi"
+            route={"/"}
+          />
+          <ViewCard
+            image="/background.png"
+            title="Ini Nama Organisasi"
+            route={"/"}
+          />
+          <ViewCard
+            image="/background.png"
+            title="Ini Nama Organisasi"
+            route={"/"}
+          />
+        </Wrap>
+      </BackgroundAndNavbar>
+    </Layout>
+  );
+}
