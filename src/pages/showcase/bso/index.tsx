fde5630f181cf20e423e96a5327f7b04e36a2acc
Nyoman-Gana <nyomanganadipa@gmail.com>
2023-08-29 00:34:42 +0700

    testing api showcase

--- src/pages/showcase/bso/index.tsx
+++ src/pages/showcase/bso/index.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import Layout from "~/layout";
 import BackgroundAndNavbar from "~/components/BackgroundAndNavbar";
 import ListPage from "~/components/listpage/ListPage";
+import { api } from "~/utils/api";
 
 export default function BSOPage() {
+  const data = api.showcase.getAllVisitedUnits.useQuery({
+    lembaga: "UKM",
+    searchValue: "ksep",
+    limit: 5,
+  });
+  console.log(data);
   return (
     <Layout title="Badan Semi Otonom">
       <BackgroundAndNavbar bg="/background.png">
