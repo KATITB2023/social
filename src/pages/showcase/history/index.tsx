55d1446177b3363fbfe63ca1ef1465279e0070a8
Gulilil <juan.csantoso@gmail.com>
2023-08-28 21:38:45 +0700

    fix : history ukm, bso, and himpunan fix

--- src/pages/showcase/history/index.tsx
+++ src/pages/showcase/history/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Wrap } from "@chakra-ui/react";
 import { ViewCard } from "~/components/showcase/ViewCard";
 import Layout from "~/layout";
-import BackgroundAndNavbar from "~/components/BackgroundAndNavbar";
 import RiwayatUnitPage from "~/components/listpage/RiwayatUnitPage";
 
 export default function HistoryPage() {
   return (
     <Layout title="Riwayat Pengunjungan">
-        <RiwayatUnitPage/>
+        <RiwayatUnitPage title=""/>
     </Layout>
   );
 }
