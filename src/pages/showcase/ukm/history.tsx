657710412f5baa1e4f8fbc1f51fdaa4270ff8c81
Nyoman-Gana <nyomanganadipa@gmail.com>
2023-08-30 19:21:58 +0700

    Feat: showcase/bso, showcase/ukm

--- src/pages/showcase/ukm/history.tsx
+++ src/pages/showcase/ukm/history.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Layout from "~/layout";
-import RiwayatUnitPage from "~/components/listpage/RiwayatUnitPage";
 import { withSession } from "~/server/auth/withSession";
 import { useSession } from "next-auth/react";
+import RiwayatPage from "~/components/listpage/RiwayatPage";
+import BackgroundAndNavbar from "~/components/BackgroundAndNavbar";
 
 export const getServerSideProps = withSession({ force: true });
 
@@ -10,7 +11,15 @@ export default function HistoryPage() {
   useSession({ required: true });
   return (
     <Layout title="Riwayat Pengunjungan">
-      <RiwayatUnitPage title="Riwayat Pengunjungan" />
+      <BackgroundAndNavbar bg="/background.png">
+        <RiwayatPage
+          title="Riwayat Pengunjungan"
+          description="Berikut adalah daftar unit yang telah kamu kunjungi."
+          withbackbutton={true}
+          lembaga="UKM"
+          withInfiniteScroll={true}
+        />
+      </BackgroundAndNavbar>
     </Layout>
   );
 }
