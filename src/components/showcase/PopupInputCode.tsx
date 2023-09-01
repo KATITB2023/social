b23f275c7eb222574acc895a1ec1bb35bb69f5a7
pandora-1 <michaelkahari.mk@gmail.com>
2023-09-02 00:02:37 +0700

    fix: fixing logic error in input popup

--- src/components/showcase/PopupInputCode.tsx
+++ src/components/showcase/PopupInputCode.tsx
@@ -23,10 +23,10 @@ export const PopupInputCode = ({
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
-    if(inputValue.length < 6) {
+    if(newValue.length < 6) {
       setInputValue(newValue);
       setIsInputValid(newValue.trim() !== "");
-    }
+    } 
   };
 
   const handleSubmit = () => {
